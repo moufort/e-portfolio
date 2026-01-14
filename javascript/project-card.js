d7239c4542ebef473f745afdce030714324b78f6
moufort <q.moutte@icloud.com>
2026-01-14 01:50:15 +0100

    move css in js to CSS file

--- javascript/project-card.js
+++ javascript/project-card.js
@@ -1,56 +1,4 @@
-const style = document.createElement('style');
-style.textContent = `
-  /* Conteneur des cartes */
-  #Projet {
-    display: grid;
-    grid-template-columns: repeat(3, 1fr);
-    gap: 30px;
-    justify-items: center;
-    padding: 20px;
-  }
-
-  /* Style de la carte */
-  .card {
-    width: 100%;
-    max-width: 20vw;
-    height: 380px;
-    border: 1px solid #ccc;
-    border-radius: 12px;
-    overflow: hidden;
-    display: flex;
-    flex-direction: column;
-    cursor: pointer;
-    transition: transform 0.2s, box-shadow 0.2s;
-    background: transparent;
-  }
-
-  .card:hover {
-    transform: translateY(-6px);
-  }
-
-  .card img {
-    width: 100%;
-    height: 80%;
-    object-fit: fill;
-  }
-
-  .card h3 {
-    height: 20%;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    font-weight: bold;
-    font-size: 1.2em;
-  }
-
-  /* Mobile */
-  @media (max-width: 600px) {
-    .card {
-      max-width: 90vw;
-    }
-  }
-`;
-document.head.appendChild(style);
+import {projets} from "./projets-data.js";
 
 const modal = document.getElementById("project-modal");
 
@@ -83,27 +31,9 @@ modal.addEventListener("click", (e) => {
   }
 });
 
-// Exemple de "base de données"
-const projets = [
-{
-    titre: "Projet Alpha",
-    image: "./image/project_picture/app.png",
-    description: "Description 1"
-},
-{
-    titre: "Projet Beta",
-    image: "./image/project_picture/Chasse_au_monstre.png",
-    description: "Description 2"
-},
-{
-    titre: "Projet Gamma",
-    image: "./image/project_picture/JSAE.png",
-    description: "Description 3"
-}
-];
-
 // Ajout automatique des cartes dans le container
 const container = document.getElementById("Projet");
+console.log( projets);
     projets.forEach(projet => {
     container.appendChild(createCard(projet));
 });
