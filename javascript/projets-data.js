d196d3a9570e010f43326477c22a56e7c3a0cfcb
moufort <q.moutte@icloud.com>
2026-01-14 02:56:00 +0100

    add a is Building page

---
+++ javascript/projets-data.js
@@ -0,0 +1,92 @@
+// Exemple de "base de données"
+export const projets = [
+
+    {
+        titre: "Shoot'em up Javascrip",
+        image: "./image/project_picture/app.png",
+        description: `Ce projet est un shoot'em up écrit en JavaScript. <br>
+                                    Dans ce jeu, nous contrôlons un personnage, et nous devons survivre aux ennemies qui apparaissent en boucle sur le jeu. <br>
+                                    Nous pouvons pour cela tirer des projectiles pour nous défendre, et récupérer des bonus pour survivre plus longtemps. <br>
+                                    Ce jeu est multijoueur, plusieurs personnes peuvent se connecter en même temps grâce à un serveur qui s'occupe de tous les calculs. <br>`,
+        link: "https://gitlab.univ-lille.fr/nathan.dorny.etu/jsae-sujet",
+        competence: "Outils : Javascript, HTML, CSS, méthode agile <br> Savoir-être acquis : Organisation"
+    },
+    {
+        titre: "API Rest",
+        image: "./image/project_picture/Chasse_au_monstre.png",
+        description: `Lors de cette SAE, nous devions mettre en place une API REST pour une pizzeria. <br>
+                                    Ainsi, nous devions des endpoint, c'est-à-dire des url qui permette de récupérer des informations ou les modifié depuis une console ou un navigateur. <br>
+                                    Nous avons aussi mis en place une sécurité avec un token d'accès. <br>`,
+        link: "https://gitlab.univ-lille.fr/nathan.dorny.etu/sae_rest_h_dorny_moutte",
+        competence: "Outils : Java, SQL, Tomcat, HTML <br> Savoir-être acquis : Organisation"
+    },
+
+
+
+
+
+
+
+
+    {
+        titre: "Shoot'em up Javascrip",
+        image: "./image/project_picture/app.png",
+        description: `Ce projet est un shoot'em up écrit en JavaScript. <br>
+                                    Dans ce jeu, nous contrôlons un personnage, et nous devons survivre aux ennemies qui apparaissent en boucle sur le jeu. <br>
+                                    Nous pouvons pour cela tirer des projectiles pour nous défendre, et récupérer des bonus pour survivre plus longtemps. <br>
+                                    Ce jeu est multijoueur, plusieurs personnes peuvent se connecter en même temps grâce à un serveur qui s'occupe de tous les calculs. <br>`,
+        link: "https://gitlab.univ-lille.fr/nathan.dorny.etu/jsae-sujet",
+        competence: "Outils : Javascript, HTML, CSS, méthode agile <br> Savoir-être acquis : Organisation"
+    },
+    {
+        titre: "API Rest",
+        image: "./image/project_picture/Chasse_au_monstre.png",
+        description: `Lors de cette SAE, nous devions mettre en place une API REST pour une pizzeria. <br>
+                                    Ainsi, nous devions des endpoint, c'est-à-dire des url qui permette de récupérer des informations ou les modifié depuis une console ou un navigateur. <br>
+                                    Nous avons aussi mis en place une sécurité avec un token d'accès. <br>`,
+        link: "https://gitlab.univ-lille.fr/nathan.dorny.etu/sae_rest_h_dorny_moutte",
+        competence: "Outils : Java, SQL, Tomcat, HTML <br> Savoir-être acquis : Organisation"
+    },
+    {
+        titre: "Chasse aux monstres",
+        image: "./image/project_picture/JSAE.png",
+        description: `Le jeu Chasse au Monstre est un jeu programmé en java où 2 joueurs s'affrontent dans un labyrinthe <br>
+                                    Un monstre, qui a pour but de s'échapper du labyrinthe qu'il connaît entièrement, <br>
+                                    et un chasseur qui doit trouver le monstre dans un labyrinthe qu'il ne connaît pas. <br>
+                                    Les joueurs jouent chacun à tour de rôle. <br>`,
+        link: "https://gitlab.univ-lille.fr/sae302/2023/H4_SAE3A",
+        competence: `Outils : Java, méthode agile <br>
+                                    Savoir-être acquis : Travail d'équipe`
+    },
+    {
+        titre: "Mini projet Agile",
+        image: "./image/project_picture/app.png",
+        description: `Le mini-projet agile visait à nous apprendre les méthodes de développement agile.<br>
+                                    Nous étions en groupe de 4 ou 5 personnes et devions coder un jeu en équipe en 3 jours.<br>`,
+        link: "https://gitlab.univ-lille.fr/2023-projet-agile-de-rentree/groupe-19",
+        competence: `Outils : Java, méthode agile <br>
+                                    Savoir-être acquis : Travail d'équipe`
+    },
+    {
+        titre: "Application d’échange internationale",
+        image: "./image/project_picture/Chasse_au_monstre.png",
+        description: `L'application d'échange était un projet de création d'application.<br>
+                                    Nous devions créer une application permettant de mettre en lien des élèves de différents pays
+                                    dans le cadre d'un échange international.<br>
+                                    Il y avait un côté élève, où l'utilisateur devait remplir un formulaire pour s'inscrire,
+                                    et un côté professeur pour mettre en relation les élèves ayant le plus d'affinités.<br>`,
+        link: "https://gitlab.univ-lille.fr/sae2.01-2.02/2023/A5",
+        competence: `Outils : Java, Javafx, fxml, SQL <br>
+                                    Savoir-être acquis : Communication`
+    },
+    {
+        titre: "Plateforme de covoiturage en ligne",
+        image: "./image/project_picture/JSAE.png",
+        description: `La plateforme de covoiturage en ligne était un projet de site Internet.<br>
+                                    Nous étions en groupe de 3 ou 4, et nous devions créer un site pour une entreprise
+                                    qui souhaitait que ses employés se déplacent en covoiturage. <br>`,
+        link: "https://gitlab.univ-lille.fr/quentin.moutte.etu/site-thales",
+        competence: `Outils : HTML, CSS <br>
+                                    Savoir-être acquis : Créativité`
+    }
+];
